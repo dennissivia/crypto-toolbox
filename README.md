2cafebfabbfcadb1f85dd6a3577d52d66d2573c6
scepticulous <dev@d-coded.de>
2015-04-01 14:48:22 +0200

    added #hex method. added proper Readme for CryptBuffer

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 # crypto-toolbox
 Small toolbox for simple Crypto analysis and learning applied cryptography techniques.
 
+## CryptBuffer
+The CryptBuffer is made to make Xor operations on strings, bytes, hex-strings easy.
 
+### Usage Examples
+#### Input Type conversion
+
+```ruby
+CryptBuffer("0xFFeecc")
+=> #<CryptBuffer:0x000000010d8e18 @bytes=[255, 238, 204]>
+
+CryptBuffer("FFeecc")
+=> #<CryptBuffer:0x000000010d8e18 @bytes=[255, 238, 204]>
+
+CryptBuffer(0xFFeecc)
+=> #<CryptBuffer:0x000000010d8e18 @bytes=[255, 238, 204]>
+
+CryptBuffer("my example String")
+=> #<CryptBuffer:0x00000000f353b8 @bytes=[109, 121, 32, 101, 120, 97, 109, 112, 108, 101, 32, 83, 116, 114, 105, 110, 103]>
+
+CryptBuffer(64)
+=> #<CryptBuffer:0x000000010d60f0 @bytes=[64]>
+```
+
+#### Simple Xoring
+
+```ruby
+key = CryptBuffer("my-super-secret-key")
+str = CryptBuffer("my-public-message!!")
+cipher = key.xor(str)
+```
+
+#### Method Chaining
+```ruby
+CryptBuffer("secret-key").xor("my message").xor_all_with(0x20).xor("secret-key").xor_all_with(0x20).str
+=> "my message"
+```
+####
+Enumerable
+
+
+#### Output conversion
+
+```ruby
+buf = CryptBuffer("0xfecc993")
+
+buf.to_s
+=> "\xFE\xCC\x99"
+buf.chars
+=> ["\xFE", "\xCC", "\x99"]
+buf.bytes
+=> [254, 204, 153]
+buf.hex
+=> "fecc99"
+
+```
+
+Shortcut methods are: 
+```ruby
+s => to_s
+h => hex
+b => bytes
+c => chars
+```
+
+#### Debug output 
+pp method:
+```ruby
+CryptBuffer("0xfecc993").pp
+=> 0xFECC99 (FE CC 99)
--- crypt_buffer.rb
+++ crypt_buffer.rb
@@ -5,50 +5,63 @@ require 'aes'
 
 class CryptBuffer
   attr_accessor :bytes
+
+  include Enumerable
+
   def initialize(input)
     @bytes = bytes_from_any(input)
   end
 
-  def b
-    bytes
+  def each(&block)
+    @bytes.each(&block)
   end
-  def h
-    bytes2hex(bytes) 
+
+  alias_method :b, :bytes
+
+  def hex
+    bytes2hex(bytes)
   end
+  alias_method :h, :hex
+  
   def chars
-    bytes.map{|b| b.to_i.chr}
+    map{|b| b.to_i.chr}
   end
   alias_method :c, :chars
+
   def str
-    bytes.map{|b| b.to_i.chr}.join
+    chars.join
   end
   alias_method :s, :str
+
   def bits
-    bytes.map{|b| "%08d" % b.to_s(2) }
+    map{|b| "%08d" % b.to_s(2) }
   end
+
   def xor(input)
     xor_bytes(bytes_from_any(input))
   end
 
   def xor_all_with(byte)
-    result = self.bytes.map{|b| b ^ byte }
+    result = map{|b| b ^ byte }
     CryptBuffer.new(result)
   end
+
   def pp
     puts pretty_hexstr
   end
 
   def xor_space
-    self.xor_all_with(0x20)
+    xor_all_with(0x20)
   end
 
   def ==(other)
-    self.bytes == bytes_from_any(other)
+    bytes == bytes_from_any(other)
   end
 
   def to_s
     str
   end
+
 private
   def bytes_from_any(input)
     case input
@@ -65,7 +78,7 @@ private
       when Fixnum
         if input.to_s(16).match(/^(0x)?[0-9a-fA-F]+$/).nil?
           # assume 0x prefixed integer
-          hex2bytes(normalize_hex(input.to_s(16)))          
+          hex2bytes(normalize_hex(input.to_s(16)))
         else
           # regular number
           [input].pack('C*').bytes
@@ -74,32 +87,44 @@ private
         raise "Unsupported input: #{input.inspect} of class #{input.class}"
     end
   end
+
   def normalize_hex(str)
     str.gsub(/(^0x|\s)/,"").upcase
   end
+
   def strip_hex_prefix(hex)
     raise "remove 0x from hexinput"
   end
+
   def xor_bytes(byt)
     len = [self.bytes.size,byt.size].min
     result = self.bytes[0...len].map.with_index{|b,i| b ^ byt[i] }
     CryptBuffer.new(result)
   end
+
   def xor_hex(hex)
-    x = hex2bytes(hex)  
+    x = hex2bytes(hex)
     xor_bytes(x)
   end
+
   def hex2bytes(hexstr)
     hexstr.scan(/../).map{|h| h.to_i(16) }
   end
+
   def str2bytes(str)
     str.bytes.to_a
   end
+
   def bytes2hex(bytes)
     bytes.map{|b| b.to_s(16)}.map{|hs| hs.length == 1 ? "0#{hs}" : hs  }.join
   end
+
   def pretty_hexstr
     str = h.scan(/.{2}/).to_a.join(" ")
    "0x#{h.upcase} (#{str.upcase})"
   end
+end
+
+def CryptBuffer(input)
+  CryptBuffer.new(input)
 end
